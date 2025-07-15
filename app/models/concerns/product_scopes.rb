2d52f4537b5efcf00ff65a1a358ea00709708c39
Felipe Cuevas <cuevas_castillo@hotmail.com>
2025-07-14 21:58:30 -0600

    Refactoring code: Improve Product#index, include Filter params on a ProductService and product_scopes

--- app/models/concerns/product_scopes.rb
+++ app/models/concerns/product_scopes.rb
@@ -1,14 +1,14 @@
 module ProductScopes
   extend ActiveSupport::Concern
 
-  include do
-    scope :by_brand, -> (brands) { where(brand: brands) }
-    scope :by_cut, -> (cuts) { where(cut: cuts) }
-    scope :by_model, -> (models) { where(model: models) }
-    scope :with_prices, -> { includes(:prices) }
+  included do
+    scope :by_brand, ->(brands) { where(brand: brands) }
+    scope :by_cut, ->(cuts) { where(cut: cuts) }
+    scope :by_model, ->(models) { where(model: models) }
+    scope :with_prices, ->() { includes(:prices) }
   end
 
-  def class_methods do
+  class_methods do
     def filter_by_attributes(filters)
       query = with_prices
 
